5bfd02ae01d03f67e3e51ce5dd84aecd95ba7b33
AlejandroHerr <ahernandezc@protonmail.com>
2019-03-24 18:22:08 +0100

    fix(data): Fix wrong data for province

--- src/services/calculateElectoralResults/index.spec.js
+++ src/services/calculateElectoralResults/index.spec.js
@@ -1,6 +1,7 @@
 import rawDataLoader from '../rawDataLoader';
 
 import calculateElectoralResults from '.';
+import { regionTypes } from '../../constants/regions';
 
 describe('src/services/calculateElectoralResults', () => {
   describe('results for CCAA', () => {
@@ -14,6 +15,7 @@ describe('src/services/calculateElectoralResults', () => {
           return [alias, seats];
         }),
       ).toEqual([
+        ['pais vasco', 2],
         ['andalucia', 23],
         ['aragon', 6],
         ['principado de asturias', 3],
@@ -27,7 +29,6 @@ describe('src/services/calculateElectoralResults', () => {
         ['galicia', 12],
         ['comunidad de madrid', 15],
         ['comunidad foral de navarra', 2],
-        ['pais vasco', 2],
         ['region de murcia', 5],
         ['la rioja', 2],
         ['comunitat valenciana', 13],
@@ -45,6 +46,7 @@ describe('src/services/calculateElectoralResults', () => {
           return [alias, seats];
         }),
       ).toEqual([
+        ['pais vasco', 3],
         ['andalucia', 20],
         ['aragon', 4],
         ['principado de asturias', 2],
@@ -58,13 +60,34 @@ describe('src/services/calculateElectoralResults', () => {
         ['galicia', 6],
         ['comunidad de madrid', 7],
         ['comunidad foral de navarra', 1],
-        ['pais vasco', 3],
         ['region de murcia', 2],
         ['la rioja', 1],
         ['comunitat valenciana', 6],
         ['ciudad de ceuta', 0],
         ['ciudad de melilla', 0],
       ]);
+    });
+  });
+  describe('results for Country', () => {
+    it('should match results for PP', () => {
+      const total = calculateElectoralResults(rawDataLoader.getResultsByType(2), rawDataLoader.getRegions());
+      expect(
+        total[regionTypes.COUNTRY].map(resultForCcaa => {
+          const { seats } = resultForCcaa.resultsByParty.find(({ id }) => id === 68);
+
+          return seats;
+        }),
+      ).toEqual([137]);
+    });
+    it('should match results for PSOE', () => {
+      const total = calculateElectoralResults(rawDataLoader.getResultsByType(2), rawDataLoader.getRegions());
+      expect(
+        total[regionTypes.COUNTRY].map(resultForCcaa => {
+          const { seats } = resultForCcaa.resultsByParty.find(({ id }) => id === 77);
+
+          return seats;
+        }),
+      ).toEqual([85]);
     });
   });
   it('should match exact snapshot for electoral district type 2', () => {
